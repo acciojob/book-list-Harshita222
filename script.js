7d0b93bea9875d282ced44ce64d97545ad03f3a9
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-07-08 15:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let inputs = document.querySelectorAll("input")
+let submit = document.querySelector("#submit")
+
+submit.addEventListener("click",()=>{
+    let body = document.querySelector("#book-list")
+    let row = body.insertRow()
+    for(let i=0;i<4;i++){
+        let cell = row.insertCell(i)
+        if(i==3){
+			let newBtn = document.createElement('button');
+			newBtn.className="delete";
+		    newBtn.innerText = 'X';
+            cell.append(newBtn)
+        }
+        else{
+            cell.textContent = `${inputs[i].value}`
+        }
+    }
+	body.append(row)
+	let delBtn = document.querySelectorAll(".delete")
+	for(let i=0;i<delBtn.length;i++){
+		delBtn[i].addEventListener("click",(event_details)=>{
+			console.log(event_details)
+			event_details.target.parentNode.parentNode.remove()
+		})
+	}
+})
